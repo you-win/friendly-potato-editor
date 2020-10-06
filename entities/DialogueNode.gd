9714d2db015da78ddccd82065987aca14b7201cd
Tim Yuen <timothy.yuen22@gmail.com>
2020-10-06 19:42:48 -0400

    serialize data

--- entities/DialogueNode.gd
+++ entities/DialogueNode.gd
@@ -9,6 +9,9 @@ var function: Resource = preload("res://entities/Function.tscn")
 ###############################################################################
 
 func _ready() -> void:
+	self.connect("close_request", self, "_on_close_request")
+	self.connect("resize_request", self, "_on_resize_request")
+	
 	$ChoicesButton/Button.connect("pressed", self, "_on_choices_button_pressed")
 	$OnEnterSetVariablesButton/Button.connect("pressed", self, "_on_variables_button_pressed")
 	$OnEnterFunctionsButton/Button.connect("pressed", self, "_on_functions_button_pressed")
@@ -16,6 +19,12 @@ func _ready() -> void:
 ###############################################################################
 # Connections                                                                 #
 ###############################################################################
+
+func _on_close_request() -> void:
+	queue_free()
+
+func _on_resize_request(new_minsize: Vector2) -> void:
+	self.rect_size = new_minsize
 
 func _on_choices_button_pressed() -> void:
 	var choice_option_instance: VBoxContainer = choice_option.instance()
--- screens/MainScreen.gd
+++ screens/MainScreen.gd
@@ -1,9 +1,16 @@
 extends Control
 
+const VARIABLE_TYPES = {
+	"INT": "INT",
+	"FLOAT": "FLOAT",
+	"STRING": "STRING",
+	"BOOL": "BOOL"
+}
+
 var dialogue_node: Resource = preload("res://entities/DialogueNode.tscn")
 var global_variable: Resource = preload("res://entities/GlobalVariable.tscn")
 
-onready var editor: GraphEdit = $MarginContainer/MarginBackground/HBoxContainer/EditorContainer/ColorRect/GraphEdit
+onready var editor: GraphEdit = $MarginContainer/HBoxContainer/EditorContainer/GraphEdit
 
 ###############################################################################
 # Builtin functions                                                           #
@@ -11,22 +18,32 @@ onready var editor: GraphEdit = $MarginContainer/MarginBackground/HBoxContainer/
 
 func _ready() -> void:
 	# Add node by button
-	$MarginContainer/MarginBackground/HBoxContainer/ToolBar/AddNodeButton.connect("pressed", self, "_on_add_node_button_pressed")
+	$MarginContainer/HBoxContainer/ToolBar/AddNodeButton.connect("pressed", self, "_on_add_node_button_pressed")
 	
 	# Filesystem
-	$MarginContainer/MarginBackground/HBoxContainer/ToolBar/SaveButton.connect("pressed", self, "_on_save_button_pressed")
-	$MarginContainer/MarginBackground/HBoxContainer/ToolBar/LoadButton.connect("pressed", self, "_on_load_button_pressed")
+	$MarginContainer/HBoxContainer/ToolBar/SaveButton.connect("pressed", self, "_on_save_button_pressed")
+	$MarginContainer/HBoxContainer/ToolBar/LoadButton.connect("pressed", self, "_on_load_button_pressed")
 	
 	# Global variables
-	$MarginContainer/MarginBackground/HBoxContainer/ToolBar/AddVariableButton/Button.connect("pressed", self, "_on_add_variable_button_pressed")
+	$MarginContainer/HBoxContainer/ToolBar/AddVariableButton/Button.connect("pressed", self, "_on_add_variable_button_pressed")
 	
 	# Quit
-	$MarginContainer/MarginBackground/HBoxContainer/ToolBar/QuitButton.connect("pressed", self, "_on_quit_button_pressed")
+	$MarginContainer/HBoxContainer/ToolBar/QuitButton.connect("pressed", self, "_on_quit_button_pressed")
 
 func _input(event: InputEvent) -> void:
-	if(Input.is_action_pressed("control") and Input.is_action_just_pressed("enter")):
-		var dialogue_node_instance: GraphNode = dialogue_node.instance()
-		editor.add_child(dialogue_node_instance)
+	if Input.is_action_pressed("control"):
+		# Add new node
+		if Input.is_action_just_pressed("enter"):
+			var dialogue_node_instance: GraphNode = dialogue_node.instance()
+			editor.add_child(dialogue_node_instance)
+		
+		# Zoom editor
+		if event.is_action_pressed("zoom_in"):
+			editor.zoom += 0.1
+			get_tree().set_input_as_handled()
+		elif event.is_action_pressed("zoom_out"):
+			editor.zoom -= 0.1
+			get_tree().set_input_as_handled()
 
 ###############################################################################
 # Connections                                                                 #
@@ -40,6 +57,8 @@ func _on_save_button_pressed() -> void:
 	var result: Dictionary = _parse_global_options()
 	result["dialogue"] = _parse_dialogue_nodes()
 	
+	print_debug(result)
+	
 	# TODO add fs dialogue save box
 
 func _on_load_button_pressed() -> void:
@@ -48,7 +67,7 @@ func _on_load_button_pressed() -> void:
 
 func _on_add_variable_button_pressed() -> void:
 	var global_variable_instance: VBoxContainer = global_variable.instance()
-	$MarginContainer/MarginBackground/HBoxContainer/ToolBar/ScrollContainer/VariablesContainer.add_child(global_variable_instance)
+	$MarginContainer/HBoxContainer/ToolBar/ScrollContainer/VariablesContainer.add_child(global_variable_instance)
 
 # TODO debug only, add a popup dialogue later?
 func _on_quit_button_pressed() -> void:
@@ -58,15 +77,123 @@ func _on_quit_button_pressed() -> void:
 # Private functions                                                           #
 ###############################################################################
 
-func _parse_dialogue_nodes() -> Dictionary:
+func _parse_global_options() -> Dictionary:
+	"""
+	Parse global options from the toolbar.
+	"""
+	print_debug("Parsing global options")
+
 	var result: Dictionary = {}
+	
+	result["name"] = $MarginContainer/HBoxContainer/ToolBar/TitleContainer/LineEdit.text
+	result["initial_dialogue_node"] = $MarginContainer/HBoxContainer/ToolBar/InitialNodeContainer/LineEdit.text
+	
+	var variables: Dictionary = {}
+	for v in $MarginContainer/HBoxContainer/ToolBar/ScrollContainer/VariablesContainer.get_children():
+		var variable_type: String = v.get_node("Type").get_node("LineEdit").text
+		
+		variables[v.get_node("Name").get_node("LineEdit").text] = {
+			"type": variable_type,
+			"value": _convert_variable_to_type(v.get_node("Value").get_node("LineEdit").text, variable_type)
+		}
+	
+	result["variables"] = variables
+	
+	print_debug("Finished parsing global options")
 	
 	return result
 
-func _parse_global_options() -> Dictionary:
+func _parse_dialogue_nodes() -> Dictionary:
+	"""
+	Loop through all dialogue nodes and parse all options.
+	"""
+	print_debug("Parsing dialogue nodes")
+	
 	var result: Dictionary = {}
 	
+	for n in editor.get_children():
+		if n is GraphNode:
+			var dialogue: Dictionary = {}
+			dialogue["node_text"] = n.get_node("Text").get_node("TextEdit").text
+			dialogue["should_end"] = n.get_node("ShouldEnd").get_node("CheckBox").pressed
+			dialogue["choices"] = _parse_choices(n.get_node("Choices").get_children())
+			dialogue["on_enter_set_variables"] = _parse_enter_exit_variables(n.get_node("OnEnterSetVariables").get_children())
+			dialogue["on_enter_functions"] = _parse_enter_exit_functions(n.get_node("OnEnterFunctions").get_children())
+			
+			result[n.get_node("Name").get_node("LineEdit").text] = dialogue
+	
+	print_debug("Finshed parsing dialogue nodes")
+	
 	return result
+
+func _parse_choices(choices: Array) -> Array:
+	"""
+	Parse each choice and return an Array of Dictionaries.
+	"""
+	var result: Array = []
+	
+	for c in choices:
+		var choice: Dictionary = {}
+		choice["choice_name"] = c.get_node("Name").get_node("LineEdit").text
+		choice["removable"] = c.get_node("Removable").get_node("CheckBox").pressed
+		choice["next_node"] = c.get_node("NextNode").get_node("LineEdit").text
+		choice["conditional"] = c.get_node("Conditional").get_node("CheckBox").pressed
+		choice["conditional_variables"] = _parse_conditional_variables(c.get_node("ConditionalVariables").get_children())
+		choice["on_exit_set_variables"] = _parse_enter_exit_variables(c.get_node("OnExitSetVariables").get_children())
+		choice["on_exit_functions"] = _parse_enter_exit_functions(c.get_node("OnExitFunctions").get_children())
+		
+		result.append(choice)
+	
+	return result
+
+func _parse_conditional_variables(conditional_variables: Array) -> Dictionary:
+	var result: Dictionary = {}
+	
+	for c in conditional_variables:
+		var conditional: Dictionary = {}
+		var conditional_name: String = c.get_node("Name").get_node("LineEdit").text
+		conditional["operator"] = c.get_node("Operator").get_node("LineEdit").text
+		
+		for child in $MarginContainer/HBoxContainer/ToolBar/ScrollContainer/VariablesContainer.get_children():
+			if conditional_name == child.get_node("Name").get_node("LineEdit").text:
+				conditional["value"] = _convert_variable_to_type(c.get_node("Value").get_node("LineEdit").text, child.get_node("Type").get_node("LineEdit").text)
+			else:
+				printerr("Conditional variable not found in Global Variable context")
+		
+		result[conditional_name] = conditional
+	
+	return result
+
+func _parse_enter_exit_variables(variables: Array) -> Dictionary:
+	var result: Dictionary = {}
+	
+	for v in variables:
+		result[v.get_node("Name").get_node("LineEdit").text] = v.get_node("Value").get_node("LineEdit").text
+	
+	return result
+
+func _parse_enter_exit_functions(functions: Array) -> Dictionary:
+	var result: Dictionary = {}
+	
+	for f in functions:
+		var params: Array = []
+		for p in f.get_node("Params").get_children():
+			params.append(_convert_variable_to_type(p.get_node("Value").get_node("LineEdit").text, p.get_node("Type").get_node("LineEdit").text))
+		
+		result[f.get_node("Function").get_node("LineEdit").text] = params
+	
+	return result
+
+func _convert_variable_to_type(value, type: String):
+	match type:
+		VARIABLE_TYPES.INT:
+			return int(value)
+		VARIABLE_TYPES.FLOAT:
+			return float(value)
+		VARIABLE_TYPES.STRING:
+			return str(value)
+		VARIABLE_TYPES.BOOL:
+			return bool(value)
 
 ###############################################################################
 # Public functions                                                            #
